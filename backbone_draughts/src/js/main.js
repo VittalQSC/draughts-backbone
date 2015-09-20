b30015b81dea0a7db2f88b87b68b0fd03ad8de2b
VittalQSC <azrial96@rambler.ru>
2015-09-20 20:54:42 +0300

    game next and prev step btn realization adding

--- backbone_draughts/src/js/main.js
+++ backbone_draughts/src/js/main.js
@@ -38,6 +38,8 @@ $(function(){
 
     App.Views.Game = Backbone.View.extend({
         brdSize: 8,
+        currentPlayerColor: 'white',
+        currentStep: 1,
         initialize: function () {
             this.createBoard(this.brdSize);
 
@@ -48,19 +50,19 @@ $(function(){
                     to: 2,
                     position: {
                         whites: [1,2,3,4,5,6,7,8,9,10,11,12],
-                        whiteQueens: [13],
+                        whiteQueens: [],
                         blacks: [32,31,30,29,28,27,26,25,24],
-                        blackQueens: [15]
+                        blackQueens: []
                     }
                 },
                 black: {
                     from: 3,
                     to: 4,
                     position: {
-                        whites: [1,2,3,4,5],
-                        whiteQueens: [6,9],
-                        blacks: [],
-                        blackQueens: [11,12,13]
+                        whites: [1,2,3,4,5,6,7,8,9,10,11,16],
+                        whiteQueens: [],
+                        blacks: [32,31,30,29,28,27,26,25,24],
+                        blackQueens: []
                     }
                 }
             }));
@@ -69,21 +71,21 @@ $(function(){
                 white: {
                     from: 5,
                     to: 6,
-                    whitePosition: {
-                        whites: [],
-                        whiteQueens: [],
-                        blacks: [],
+                    position: {
+                        whites: [1,2,3],
+                        whiteQueens: [5,6],
+                        blacks: [23,15],
                         blackQueens: []
                     }
                 },
                 black: {
                     from: 8,
                     to: 7,
-                    blackPosition: {
+                    position: {
                         whites: [],
                         whiteQueens: [],
-                        blacks: [],
-                        blackQueens: []
+                        blacks: [1,2,3,4],
+                        blackQueens: [23,5]
                     }
                 }
             }));
@@ -92,17 +94,17 @@ $(function(){
             this.fillGameStepsSidebar();
             this.render(1,'white');
         },
-        fillGameStepsSidebar: function(){
+        fillGameStepsSidebar: function () {
             for(var i = 0; i < this.collection.length; i++) {
                 this.insertHalfStepsIntoStepsSidebar(this.collection.at(i));
             }
         },
-        insertHalfStepsIntoStepsSidebar: function(step){
+        insertHalfStepsIntoStepsSidebar: function (step) {
             var halfSteps = this.determineStep(step);
             $('#game-steps').append('<li id="' + step.get('index') + '-step" class="step">'+halfSteps.whiteHalfStep+'</li>');
             $('#game-steps').append('<li id="' + step.get('index') + '-step" class="step">'+halfSteps.blackHalfStep+'</li>');
         },
-        determineStep: function(step) {
+        determineStep: function (step) {
             var answer = {
                 whiteHalfStep: {},
                 blackHalfStep: {}
@@ -129,7 +131,7 @@ $(function(){
             }
             return answer;
         },
-        convert10x10to8x8: function(coordinate10x10){
+        convert10x10to8x8: function (coordinate10x10) {
             //hor array just a wow mind games 9k
             var hor = ['A','C','E','G','B','D','F','H'];
             var verticalCoordinate = Math.floor((coordinate10x10-1)/4) + 1;
@@ -141,10 +143,10 @@ $(function(){
             };
 // 9 3A
         },
-        convert8x8to10x10: function(coordinate8x8){
+        convert8x8to10x10: function (coordinate8x8) {
             // is not working now
         },
-        createBoard: function(brdSize) {
+        createBoard: function (brdSize) {
             var firstSquareIsWhite;
             for(var i = brdSize; i >= 1; i--) {
                 $('#draughts-board').append('<div id="'+ i +'-row" class="'+ this.getRowClassName() +'"></div>');
@@ -152,7 +154,7 @@ $(function(){
                 for(var j = 1; j < brdSize + 1; j++) {
                     var squareColor = this.determineSquareColor(firstSquareIsWhite,j);
                     var squareCoordinate =  this.determineSquareCoordinate(i,j);
-                    var squareClassName = this.getSquareClassName(squareCoordinate);
+                    var squareClassName = this.getSquareClassName();
                     if(squareColor === 'black')
                         $('#'+ i +'-row').append('<div id="'+ squareCoordinate +'-square" class="' + squareClassName + ' '+squareColor+'-square"></div>');
                     else
@@ -160,7 +162,7 @@ $(function(){
                 }
             }
         },
-        determineSquareColor: function (firstSquareIsWhite,j){
+        determineSquareColor: function (firstSquareIsWhite,j) {
         if(firstSquareIsWhite)
             if( j % 2 != 0)
                 return "white";
@@ -173,45 +175,63 @@ $(function(){
                 return "white";
 
     },
-        determineSquareCoordinate: function(i,j) {
+        determineSquareCoordinate: function (i,j) {
             return i.toString() + String.fromCharCode(64+j);
         },
-        getSquareClassName: function(squareCoordinate) {
+        getSquareClassName: function () {
             return 'board8x8-square';
         },
-        getRowClassName: function() {
+        getRowClassName: function () {
             return 'row8x8';
         },
         events: {
             "click #next-step"   : "nextStep",
             "click #prev-step"   : "prevStep"
         },
-        colorCurrentStep: function(){
-            $('.step').removeClass('curr-step');
-            if(this.collection.currStepNum != 0) {
-                $('#'+ this.collection.currStepNum +'-step').addClass('curr-step');
+        nextStep: function () {
+            if(this.currentStep <= this.collection.length) {
+                if(this.currentPlayerColor === 'white') {
+                    this.currentPlayerColor = 'black';
+                    this.render(this.currentStep, this.currentPlayerColor);
+                }
+                else if(this.currentStep < this.collection.length) {
+                    this.currentPlayerColor = 'white';
+                    this.render(++this.currentStep, this.currentPlayerColor);
+                }
             }
         },
-        nextStep: function(){
-            if(this.collection.currStepNum < this.collection.length - 1) this.collection.currStepNum++;
-            this.render();
-        },
-        prevStep: function(){
-            if(this.collection.currStepNum > 0) this.collection.currStepNum--;
-            this.render();
+        prevStep: function () {
+            if(this.currentStep >= 1) {
+                if(this.currentPlayerColor === 'white' && this.currentStep > 1) {
+                    this.currentPlayerColor = 'black';
+                    this.render(--this.currentStep,'black');
+                }
+                else {
+                    this.currentPlayerColor = 'white';
+                    this.render(this.currentStep,'white');
+                }
+            }
         },
         putFigure: function (coordinate10x10, className) {
             var coordinate8x8 = this.convert10x10to8x8(coordinate10x10);
-            //console.log(coordinate8x8.verticalCoordinate+coordinate8x8.horizontalCoordinate + " : " + className);
             $('#'+coordinate8x8.verticalCoordinate+coordinate8x8.horizontalCoordinate+'-square').addClass(className);
         },
-        render: function(index, playerColor){
+        clearSquare: function (i) {
+            var coordinate8x8 = this.convert10x10to8x8(i);
+            $('#'+coordinate8x8.verticalCoordinate+coordinate8x8.horizontalCoordinate+'-square').removeClass('white-figure');
+            $('#'+coordinate8x8.verticalCoordinate+coordinate8x8.horizontalCoordinate+'-square').removeClass('black-figure');
+            $('#'+coordinate8x8.verticalCoordinate+coordinate8x8.horizontalCoordinate+'-square').removeClass('white-queen');
+            $('#'+coordinate8x8.verticalCoordinate+coordinate8x8.horizontalCoordinate+'-square').removeClass('black-queen');
+        },
+        render: function (index, playerColor) {
             var playerPosition = this.collection.at(index-1).get(playerColor).position;
-
             var whites = playerPosition.whites;
             var blacks = playerPosition.blacks;
             var whiteQueens = playerPosition.whiteQueens;
             var blackQueens = playerPosition.blackQueens;
+            for(var i = 0; i < this.brdSize*this.brdSize/2; i++) {
+                this.clearSquare(i);
+            }
 
             for(var i = 0; i < whites.length; i++) {
                 this.putFigure(whites[i],'white-figure');
@@ -225,15 +245,7 @@ $(function(){
             for(var i = 0; i < blackQueens.length; i++) {
                 this.putFigure(blackQueens[i],'black-queen');
             }
-        },
-        determineFigure: function(square) {
-            if(square == 1) return "white-figure";
-            else if(square == 2) return "black-figure";
-            else if(square == 3) return "white-queen";
-            else if(square == 4) return "black-queen";
-            else if(square == 0) return "empty";
-            else return "empty";
-         }
+        }
     });
 
     App.Views.Game10x10 = App.Views.Game.extend({
@@ -266,6 +278,12 @@ $(function(){
         },
         putFigure: function (coordinate10x10, className) {
             $('#'+coordinate10x10.toString()+'-square').addClass(className);
+        },
+        clearSquare: function (i) {
+            $('#' + i + '-square').removeClass('white-figure');
+            $('#' + i + '-square').removeClass('black-figure');
+            $('#' + i + '-square').removeClass('white-queen');
+            $('#' + i + '-square').removeClass('black-queen');
         }
     });
 
